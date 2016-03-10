5df08d033a401ff664a50231c4e4bb11cd5c2f13
Ben Densmore <bdensmore@densmore.net>
2016-03-10 20:36:14 +0000

    Convert GET /todos to sequelize

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var db = require('./db.js');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [];
+// var todos = [];
 var todoNextId = 1;
 
 app.use(bodyParser.json());
@@ -15,22 +15,34 @@ app.get('/', function(req, res) {
 });
 
 app.get('/todos', function(req, res) {
-  var queryParams = req.query;
-  var filteredTodos = todos;
+  var query = req.query;
+  var where = {};
   
-  if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
-    filteredTodos = _.where(filteredTodos, {completed: true});
-  } else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
-    filteredTodos = _.where(filteredTodos, {completed: false});
+  if (query.hasOwnProperty('completed') && query.completed === 'true') {
+    where.completed = true;
+  } else if (query.hasOwnProperty('completed') && query.completed === 'false') {
+    where.completed = false;
   }
   
-  if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
-    filteredTodos = _.filter(filteredTodos, function(todo) { 
-      return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
-    });
+  if (query.hasOwnProperty('q') && query.q.length > 0) {
+    where.description =  { $like: '%' + query.q + '%' };
   }
+
   
-  res.json(filteredTodos);
+  db.todo.findAll({
+    where: where
+  }).then(function(todos) {
+    if (!!todos) {
+      res.json(todos);
+      // todos.forEach(function(todo) {
+      //   res.json(todo.toJSON()); 
+      // });
+    } else { 
+      res.status(404).send()
+    }
+  }, function(e) {
+    res.status(500).json(e);
+  });
 });
 
 app.get('/todos/:id', function(req, res) {
@@ -43,7 +55,7 @@ app.get('/todos/:id', function(req, res) {
       res.status(404).send();
     }
   }, function(e) {
-      res.status(400).json(e);
+      res.status(500).json(e);
   });
    
 });
